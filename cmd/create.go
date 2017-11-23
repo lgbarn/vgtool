da5b963ab325330a42c084d75317da904509c442
Luther Barnum <luther.barnum@gmail.com>
2017-11-22 23:42:13 -0500

    Added create command

--- cmd/create.go
+++ cmd/create.go
@@ -16,6 +16,8 @@ package cmd
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -31,7 +33,57 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("create called")
+		var vgName string
+		var lvName string
+		var lvPath string
+		var lvSize string
+		var lvols = make([]*lvol, 0, 50)
+		var pvDisks = make([]string, 0, 50)
+		var vgNameRE, _ = regexp.Compile(`VG Name\s+(\w+)`)
+		var lvNameRE, _ = regexp.Compile(`LV Name\s+(.+)`)
+		var lvSizeRE, _ = regexp.Compile(`LV Size\s+(.+)`)
+		var pvNameRE, _ = regexp.Compile(`PV Name\s+(.+)`)
+
+		lines, err := ParseLines(file, func(s string) (string, bool) {
+			return s, true
+		})
+		if err != nil {
+			fmt.Println("Error while parsing file", err)
+			return
+		}
+
+		for _, fileLine := range lines {
+			//fmt.Println(fileLine)
+			switch {
+			case vgNameRE.MatchString(fileLine):
+				vgName = vgNameRE.FindStringSubmatch(fileLine)[1]
+			case lvNameRE.MatchString(fileLine):
+				lvName = lvNameRE.FindStringSubmatch(fileLine)[1]
+			case lvSizeRE.MatchString(fileLine):
+				lvSize = lvSizeRE.FindStringSubmatch(fileLine)[1]
+				cleanlvSize := strings.Replace(lvSize, " ", "", -1)
+				if targetvgPtr != "" {
+					lvPath = strings.Replace(lvPath, vgName, targetvgPtr, -1)
+					vgName = targetvgPtr
+				}
+				newlvol := &lvol{lvName: lvName, vgName: vgName, lvPath: lvPath, lvSize: cleanlvSize}
+				lvols = append(lvols, newlvol)
+			case pvNameRE.MatchString(fileLine):
+				pvName := pvNameRE.FindStringSubmatch(fileLine)[1]
+				cleanDisk := strings.TrimSpace(pvName)
+				pvDisks = append(pvDisks, cleanDisk)
+			}
+		}
+
+		newvg := &vg{vgName: vgName, disks: pvDisks}
+		newvg.vgCreate()
+		for _, currLvol := range lvols {
+			currLvol.lvCreate()
+		}
+
+		//for _, currLvol := range lvols {
+		//	currLvol.lvExtend()
+		//}
 	},
 }
 
@@ -47,4 +99,6 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// createCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	createCmd.Flags().StringVarP(&file, "file", "f", "", "Help message for toggle")
+	createCmd.Flags().StringVarP(&targetvgPtr, "target", "t", "", "Help message for toggle")
 }
