32a37005dc5aa4862e7f3cc3fe5dae914d29d26f
Luther Barnum <luther.barnum@gmail.com>
2017-11-22 22:41:30 -0500

    Completed initial extend command

--- cmd/extend.go
+++ cmd/extend.go
@@ -17,7 +17,6 @@ package cmd
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
 	"regexp"
 	"strings"
@@ -29,6 +28,17 @@ var file string
 var filePtr *string
 var targetvgPtr string
 var lvextendPtr string
+var fileLine string
+var vgName string
+var lvName string
+var lvPath string
+var lvSize string
+var lvols = make([]*lvol, 0, 50)
+var pvDisks = make([]string, 0, 50)
+var vgNameRE, _ = regexp.Compile(`VG Name\s+(\w+)`)
+var lvNameRE, _ = regexp.Compile(`LV Name\s+(.+)`)
+var lvSizeRE, _ = regexp.Compile(`LV Size\s+(.+)`)
+var pvNameRE, _ = regexp.Compile(`PV Name\s+(.+)`)
 
 type lvol struct {
 	lvPath     string
@@ -45,8 +55,7 @@ type vg struct {
 }
 
 func (Lvol *lvol) lvExtend() {
-	fmt.Printf("lvextend -L %s -n %s\n", Lvol.lvSize, Lvol.lvPath)
-	fmt.Printf("fsadm resize %s\n", Lvol.lvPath)
+	fmt.Printf("lvextend -r -L %s -n %s\n", Lvol.lvSize, Lvol.lvName)
 }
 func (VG *vg) vgCreate() {
 	disk := strings.Join(VG.disks[:], " ")
@@ -54,6 +63,26 @@ func (VG *vg) vgCreate() {
 }
 func (Lvol *lvol) lvCreate() {
 	fmt.Printf("lvcreate -L %s -n %s %s\n", Lvol.lvSize, Lvol.lvName, Lvol.vgName)
+}
+
+func ParseLines(filePath string, parse func(string) (string, bool)) ([]string, error) {
+	inputFile, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer inputFile.Close()
+
+	scanner := bufio.NewScanner(inputFile)
+	var results []string
+	for scanner.Scan() {
+		if output, add := parse(scanner.Text()); add {
+			results = append(results, output)
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+	return results, nil
 }
 
 // extendCmd represents the extend command
@@ -67,61 +96,43 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("extend called")
-		fmt.Println(file)
 
-		var fileLine string
-		var vgName string
-		var lvName string
-		var lvPath string
-		var lvSize string
-		lvols := make([]*lvol, 0, 50)
-		pvDisks := make([]string, 0, 50)
-
-		vgNameRE, _ := regexp.Compile(`VG Name\s+(\w+)`)
-		lvNameRE, _ := regexp.Compile(`LV Name\s+(\w+)`)
-		lvSizeRE, _ := regexp.Compile(`LV Size\s+(.+)`)
-		lvPathRE, _ := regexp.Compile(`LV Path\s+(.+)`)
-		pvNameRE, _ := regexp.Compile(`PV Name\s+(.+)`)
-		filePtr, err := os.Open(file)
+		lines, err := ParseLines(file, func(s string) (string, bool) {
+			return s, true
+		})
 		if err != nil {
-			log.Fatal(err)
+			fmt.Println("Error while parsing file", err)
+			return
 		}
-		fileScanner := bufio.NewScanner(filePtr)
-		for fileScanner.Scan() {
-			fileLine = fileScanner.Text()
-			if err := fileScanner.Err(); err != nil {
-				fmt.Fprintln(os.Stderr, "reading file:", err)
-			} else {
-				switch {
-				case vgNameRE.MatchString(fileLine):
-					vgName = vgNameRE.FindStringSubmatch(fileLine)[1]
-				case lvNameRE.MatchString(fileLine):
-					lvName = lvNameRE.FindStringSubmatch(fileLine)[1]
-				case lvPathRE.MatchString(fileLine):
-					lvPath = lvPathRE.FindStringSubmatch(fileLine)[1]
-				case lvSizeRE.MatchString(fileLine):
-					lvSize = lvSizeRE.FindStringSubmatch(fileLine)[1]
-					cleanlvSize := strings.Replace(lvSize, " ", "", -1)
-					if targetvgPtr != "" {
-						lvPath = strings.Replace(lvPath, vgName, targetvgPtr, -1)
-						vgName = targetvgPtr
-					}
-					newlvol := &lvol{lvName: lvName, vgName: vgName, lvPath: lvPath, lvSize: cleanlvSize}
-					lvols = append(lvols, newlvol)
-				case pvNameRE.MatchString(fileLine):
-					pvName := pvNameRE.FindStringSubmatch(fileLine)[1]
-					cleanDisk := strings.TrimSpace(pvName)
-					pvDisks = append(pvDisks, cleanDisk)
+
+		for _, fileLine := range lines {
+			//fmt.Println(fileLine)
+			switch {
+			case vgNameRE.MatchString(fileLine):
+				vgName = vgNameRE.FindStringSubmatch(fileLine)[1]
+			case lvNameRE.MatchString(fileLine):
+				lvName = lvNameRE.FindStringSubmatch(fileLine)[1]
+			case lvSizeRE.MatchString(fileLine):
+				lvSize = lvSizeRE.FindStringSubmatch(fileLine)[1]
+				cleanlvSize := strings.Replace(lvSize, " ", "", -1)
+				if targetvgPtr != "" {
+					lvPath = strings.Replace(lvPath, vgName, targetvgPtr, -1)
+					vgName = targetvgPtr
 				}
+				newlvol := &lvol{lvName: lvName, vgName: vgName, lvPath: lvPath, lvSize: cleanlvSize}
+				lvols = append(lvols, newlvol)
+			case pvNameRE.MatchString(fileLine):
+				pvName := pvNameRE.FindStringSubmatch(fileLine)[1]
+				cleanDisk := strings.TrimSpace(pvName)
+				pvDisks = append(pvDisks, cleanDisk)
 			}
 		}
 
-		newvg := &vg{vgName: vgName, disks: pvDisks}
-		newvg.vgCreate()
-		for _, currLvol := range lvols {
-			currLvol.lvCreate()
-		}
+		//newvg := &vg{vgName: vgName, disks: pvDisks}
+		//newvg.vgCreate()
+		//for _, currLvol := range lvols {
+		//	currLvol.lvCreate()
+		//}
 
 		for _, currLvol := range lvols {
 			currLvol.lvExtend()
